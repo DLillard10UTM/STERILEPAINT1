719369f2841b6a63c367a26b0f1f5d6b473231ba
Daniel Lillard <danllill@ut.utm.edu>
2020-02-13 20:18:35 -0600

    Add project files.

---
+++ STERILEPAINT1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Author: Danny Lillard
+//Date: 1/27/2020
+namespace STERILEPAINT1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            InkCanvas.Strokes.Clear();
+        }
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            InkCanvas.DefaultDrawingAttributes.Width = BrushSizeSlider.Value;
+            InkCanvas.DefaultDrawingAttributes.Height = BrushSizeSlider.Value;
+        }
+        //Based on the slider value from 0 to 255, we change the color. Alpha is currently always set to max.
+        private void BrushColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            InkCanvas.DefaultDrawingAttributes.Color = Color.FromArgb(255, Convert.ToByte(RedColorSlider.Value), Convert.ToByte(GreenColorSlider.Value),
+                Convert.ToByte(BlueColorSlider.Value));
+        }
+    }
+}
---
+++ STERILEPAINT1/OwnMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace STERILEPAINT1
+{
+    class OwnMain
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            STERILEPAINT1.App app = new STERILEPAINT1.App();
+            app.InitializeComponent();
+
+            Window win = new Window();
+            TextBox text = new TextBox();
+            text.Name = "Textbox";
+            text.Text = "B - B - B - Bennie and the Jets\n" +
+                "Oh but they're weird and they're wonderful\n" +
+                "Oh Bennie she's really keen\n" +
+                "She's got electric boots a mohair suit\n" +
+                "You know I read it in a magazine\n" +
+                "B - B - B - Bennie and the Jets\n" +
+                "And the changes, dont want to be pushed at all.";
+
+            win.Content = text;
+            win.Show();
+            app.Run();
+        }
+    }
+}
